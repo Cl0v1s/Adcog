bb168981c639ebcf9ab56bac5ac34ae4e7d65330
Antoine Letréguilly <antoine.letreguilly@ensc.fr>
2017-03-27 21:06:07 +0200

    service checkDoubles

--- src/Adcog/UserBundle/Service/CheckDoubles.php
+++ src/Adcog/UserBundle/Service/CheckDoubles.php
@@ -2,28 +2,28 @@
 
 namespace Adcog\UserBundle\Service;
 
-use Adcog\DefaultBundle\Entity\Country
-use Adcog\DefaultBundle\Entity\City
-use Adcog\DefaultBundle\Entity\Employer
-use Adcog\DefaultBundle\Entity\Experience
-use Adcog\DefaultBundle\Entity\Sector
-use Adcog\DefaultBundle\Entity\User
-
+use Adcog\DefaultBundle\Entity\Country;
+use Adcog\DefaultBundle\Entity\City;
+use Adcog\DefaultBundle\Entity\Employer;
+use Adcog\DefaultBundle\Entity\Experience;
+use Adcog\DefaultBundle\Entity\Sector;
+use Adcog\DefaultBundle\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Adcog\DefaultBundle\Functions\StringFunc;
 
 class CheckDoubles
 {
     protected $em; 
+    protected $strfunc;
 
 	public function checkEmployerDoubles(Experience $experience)
     {
-        $em = $this->get('doctrine.orm.default_entity_manager');
-        $strfunc = $this->get('adcog.functions.string');
-        $empRep = $em->getRepository('AdcogDefaultBundle:Employer');
+        $empRep = $this->em->getRepository('AdcogDefaultBundle:Employer');
         //at this place, we do not make a case insensitive or accent insensitive search. However, the utf8_unicode_ci interclassment makes so that it is ignored.
         $employers = $empRep->findByName($experience->getEmployer()->getName());
         foreach($employers as $oneEmployer)
         {
-            if (strtolower($strfunc->remove_accents($oneEmployer->getName())) == strtolower($strfunc->remove_accents($experience->getEmployer()->getName())))
+            if (strtolower($this->strfunc->remove_accents($oneEmployer->getName())) == strtolower($this->strfunc->remove_accents($experience->getEmployer()->getName())))
             {
                 $experience->setEmployer($oneEmployer);
                 return true;
@@ -35,16 +35,14 @@ class CheckDoubles
     public function checkSectorsDoubles(Experience $experience)
     {
         $nbOfSectorsReplaced = 0;
-        $em = $this->get('doctrine.orm.default_entity_manager');
-        $strfunc = $this->get('adcog.functions.string');
-        $sectRep = $em->getRepository('AdcogDefaultBundle:Sector');
+        $sectRep = $this->em->getRepository('AdcogDefaultBundle:Sector');
         $expSectors = $experience->getSectors();
         foreach($expSectors as $sectNumber => $oneExpSector)
         {
             $sectors = $sectRep->findByName($oneExpSector->getName());
             foreach($sectors as $oneSector)
             {
-                if (strtolower($strfunc->remove_accents($oneSector->getName())) == strtolower($strfunc->remove_accents($oneExpSector->getName())))
+                if (strtolower($this->strfunc->remove_accents($oneSector->getName())) == strtolower($this->strfunc->remove_accents($oneExpSector->getName())))
                 {
                     $expSectors[$sectNumber] = $oneSector;
                     $nbOfSectorsReplaced++;
@@ -57,13 +55,12 @@ class CheckDoubles
 
     public function checkCountryDoubles(Experience $experience)
     {
-        $strfunc = $this->get('adcog.functions.string');
-        $empRep = $this->get('doctrine.orm.default_entity_manager')->getRepository('AdcogDefaultBundle:Employer');
+        $empRep = $this->em->getRepository('AdcogDefaultBundle:Employer');
         //at this place, we do not make a case insensitive or accent insensitive search. However, the utf8_unicode_ci interclassment makes so that it is ignored.
         $employers = $empRep->findByCountry($experience->getEmployer()->getCountry());
         foreach($employers as $oneEmployer)
         {
-            if (strtolower($strfunc->remove_accents($oneEmployer->getCountry())) == strtolower($strfunc->remove_accents($experience->getEmployer()->getCountry())))
+            if (strtolower($this->strfunc->remove_accents($oneEmployer->getCountry())) == strtolower($this->strfunc->remove_accents($experience->getEmployer()->getCountry())))
             {
                 $experience->getEmployer()->setCountry($oneEmployer->getCountry());                
                 return true;
@@ -73,13 +70,12 @@ class CheckDoubles
     }
     public function checkCityDoubles(Experience $experience)
     {
-        $strfunc = $this->get('adcog.functions.string');
-        $empRep = $this->get('doctrine.orm.default_entity_manager')->getRepository('AdcogDefaultBundle:Employer');
+        $empRep = $this->em->getRepository('AdcogDefaultBundle:Employer');
         //at this place, we do not make a case insensitive or accent insensitive search. However, the utf8_unicode_ci interclassment makes so that it is ignored.
         $employers = $empRep->findByCity($experience->getEmployer()->getCity());
         foreach($employers as $oneEmployer)
         {
-            if (strtolower($strfunc->remove_accents($oneEmployer->getCity())) == strtolower($strfunc->remove_accents($experience->getEmployer()->getCity())))
+            if (strtolower($this->strfunc->remove_accents($oneEmployer->getCity())) == strtolower($this->strfunc->remove_accents($experience->getEmployer()->getCity())))
             {
                 $experience->getEmployer()->setCity($oneEmployer->getCity());
                 return true;
@@ -90,5 +86,6 @@ class CheckDoubles
     public function __construct(EntityManagerInterface $em)
     {
         $this->em = $em;
+        $this->strfunc = new StringFunc();
     }
 }
